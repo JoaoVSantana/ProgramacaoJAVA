b7a0bb553872304a37ca01f0c0aaa3cc6c85da12
JoaoVSantana <joao10victorsantana@gmail.com>
2023-06-20 21:54:51 -0300

    aa

--- Ecommerce/src/model/Carrinho.java
+++ Ecommerce/src/model/Carrinho.java
@@ -1,12 +1,32 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Carrinho {
 	
-	private Cliente cliente;
-	private int quantidade;
+//	private Cliente cliente;
+	private ArrayList <Produto> produto = new ArrayList<>();
+	private ArrayList <Integer> quantidade = new ArrayList<Integer>();
 	private double valorTotal;
 	
-	public Carrinho (Cliente cliente) {
+//	public Carrinho (Cliente cliente) {
+//		setCliente(cliente);
+//	}
+	
+	public Carrinho() {
+		
+	}
+	
+//	public void setCliente(Cliente cliente) {
+//		this.cliente = cliente;
+//	}
+	
+	public void addCarrinho (Integer cquantidade, Produto cproduto) {
+		produto.add(cproduto);
+		quantidade.add(cquantidade);
+	}
+	
+	public void removeCarrinho (Integer cquantidade, Produto cproduto) {
 		
 	}
 
